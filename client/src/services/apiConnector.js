2f9501aef17b30d95b78945479eb41bc86f2627f
JaiswalShivang <jaiswalshivang01@gmail.com>
2025-06-22 19:45:19 +0530

    Update apiConnector.js

--- client/src/services/apiConnector.js
+++ client/src/services/apiConnector.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:4000/api',
+  baseURL: 'https://xyz-terb.onrender.com',
 })
 
 // Add request interceptor to include auth token
